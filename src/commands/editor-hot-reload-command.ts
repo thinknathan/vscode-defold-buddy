146207c0c637d492ff2208c8a52efc30a69b4da9
thinknathan <github@thinknathan.com>
2024-05-13 22:36:21 -0700

    feat: add support for hot reload when saving typescript

--- src/commands/editor-hot-reload-command.ts
+++ src/commands/editor-hot-reload-command.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { DefoldEditor, EditorCommand } from '../editor/defold-editor';
 
-const fileExtensionsThatTriggerHotReload = ['.script', '.lua', '.gui_script'];
+const fileExtensionsThatTriggerHotReload = ['.script', '.lua', '.gui_script', '.ts'];
 
 export function registerEditorHotReloadCommand(context: vscode.ExtensionContext) {
     // TODO: move outside of the command
@@ -9,6 +9,12 @@ export function registerEditorHotReloadCommand(context: vscode.ExtensionContext)
         if (fileExtensionsThatTriggerHotReload.some((ext) => document.fileName.endsWith(ext))) {
             const editor = new DefoldEditor(context);
             editor.showRunningDefoldEditorNotFoundWindow = false;
+
+            // If we have a TypeScript file, wait an arbitrary amount of time
+            // so the transpiler has time to write the file
+            if (document.fileName.endsWith('.ts')) {
+                await sleep(300);
+            }
             await editor.executeCommand(EditorCommand.hotReload);
         }
     });
@@ -18,3 +24,9 @@ export function registerEditorHotReloadCommand(context: vscode.ExtensionContext)
         await editor.executeCommand(EditorCommand.hotReload);
     }));
 };
+
+function sleep(ms: number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
